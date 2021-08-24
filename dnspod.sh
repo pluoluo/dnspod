207949a44f6bc4bb97ec1d1daa6795913121b4f9
pluoluo <32254253+pluoluo@users.noreply.github.com>
2021-08-24 11:05:48 +0800

    Update dnspod.sh

--- dnspod.sh
+++ dnspod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ###   脚本绝对路径
 SOURCE="$0"
-while [ -h "$SOURCE"  ]; do # resolve $SOURCE until the file is no longer a symlink
-    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+while [ -h "$SOURCE"  ]; do # 解析 $SOURCE 脚本真实路径而非软链接
+    path="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
     SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $SOURCE != /*  ]] && SOURCE="$path/$SOURCE" 
 done
 path="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"                               #脚本绝对路径
 cd $path							#以下使用相对路径
